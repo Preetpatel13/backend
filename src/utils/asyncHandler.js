40f687b7862c38aab99ecf122463ecb1cbc45fd3
Preetpatel13 <preetpatel1356@gmail.com>
2024-01-26 11:44:55 +0530

    util code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,13 @@
+const asyncHandler = (fn)=>async(req,res,next)=>{
+
+    try{
+await fn(req,res,next);
+    }
+    catch(error){
+req.status(err.code || 500).json({
+success:false,
+message:err.message})
+    }
+}
+
+export {asyncHandler}
